1c30d991cbef9f0acd372bef6400ce2403d26353
calmc0816-debug <calmc0816@gmail.com>
2025-11-10 16:59:21 -0700

    Update and rename Callum.md to Callum.cpp

---
+++ Callum.cpp
@@ -0,0 +1,19 @@
+#H1 this is Callum's folder to edit 
+> this is quote block
+> it can spam a few lines 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 0; j <= 5; j++)
+        {
+            cout << i * j << " ";
+        }
+        cout << endl;
+    }
+}
